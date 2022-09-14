5d04d393846695ec6f633260059c35787e14218a
Ali C <ali.ch.new@gmail.com>
2022-09-14 18:13:15 +0200

    testing VSCode commit

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
 
 /*** 
   Create the array of quote objects and name it `quotes`.
