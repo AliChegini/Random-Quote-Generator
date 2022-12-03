ca646e523fea1cf8925fe90d812d772aeb73b8fe
AliChegini <>
2022-12-03 17:26:04 +0100

    completed functions

--- js/script.js
+++ js/script.js
@@ -16,7 +16,8 @@ let quotes = [
     year: 1950
   },
   {
-    quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    quote: "To be yourself in a world that is constantly " + 
+    "trying to make you something else is the greatest accomplishment.",
     source: "Ralph Waldo Emerson"
   },
   {
@@ -41,39 +42,19 @@ function getRandomQuote() {
   return quotes[randomNumber];
 }
 
-//console.log(getRandomQuote());
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
 
 function printQuote() {
   let randomQuote = getRandomQuote()
-  let htmlString = "";
-  
-
+  let htmlString = '';
+  htmlString = `
+    <p class="quote"> ${randomQuote.quote} </p>
+    <p class="source"> ${randomQuote.source}
+      <span class="citation"> ${randomQuote.citation} </span>
+      <span class="year"> ${randomQuote.year} </span>
+    </p>
+  `;
+  console.log(randomQuote);
+  document.getElementById('quote-box').innerHTML = htmlString;
 }
 
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
-//console.log(getRandomQuote(quotes));
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
